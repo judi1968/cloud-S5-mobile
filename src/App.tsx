c3116585b9f6abc1a522919bc6fef9bf2fd7e5e9
Judi <judiherinirina04@gmail.com>
2024-01-18 13:40:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Route, useLocation } from 'react-router-dom';
-import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, setupIonicReact } from '@ionic/react';
+import { IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, setupIonicReact, useIonRouter } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 
@@ -26,11 +26,12 @@ import CreerCompte from './pages/CreerCompte';
 import Accueil from './pages/Accueil';
 import CreerAnnonce from './pages/CreerAnnonce';
 import { library, playCircle, radio, search } from 'ionicons/icons';
+// import { useEffect } from 'react';
 
 setupIonicReact();
 
 const App: React.FC = () => {
-
+  const ionRouter = useIonRouter();
   return(
   
   <IonApp>
@@ -38,15 +39,11 @@ const App: React.FC = () => {
     <IonTabs>
         <IonRouterOutlet>
           <Redirect exact path="/" to="/home" />
-          {/*
-          Use the render method to reduce the number of renders your component will have due to a route change.
-
-          Use the component prop when your component depends on the RouterComponentProps passed in automatically.
-        */}
+  
           <Route path="/home" render={() => <Accueil />} exact={true} />
-          <Route path="/radio" render={() => <Login />} exact={true} />
-          <Route path="/library" render={() => <CreerCompte />} exact={true} />
-          <Route path="/search" render={() => <CreerAnnonce />} exact={true} />
+          <Route path="/login" render={() => <Login />} exact={true} />
+          <Route path="/creer_compte" render={() => <CreerCompte />} exact={true} />
+          <Route path="/creer_annonce" render={() => <CreerAnnonce />} exact={true} />
         </IonRouterOutlet>
 
         <IonTabBar slot="bottom">
@@ -60,14 +57,14 @@ const App: React.FC = () => {
             <IonLabel>Radio</IonLabel>
           </IonTabButton>
 
-          <IonTabButton tab="library" href="/library">
+          <IonTabButton tab="creer_annonce" href="/creer_annonce">
             <IonIcon icon={library} />
-            <IonLabel>Library</IonLabel>
+            <IonLabel>Creer annonce</IonLabel>
           </IonTabButton>
 
-          <IonTabButton tab="search" href="/search">
+          <IonTabButton tab="login" href="/login">
             <IonIcon icon={search} />
-            <IonLabel>Search</IonLabel>
+            <IonLabel>Login</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
@@ -76,22 +73,3 @@ const App: React.FC = () => {
 )};
 
 export default App;
-
-{/* <Route exact path="/home">
-          
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/creer_compte">
-          <CreerCompte />
-        </Route>
-        <Route exact path="/accueil">
-          <Accueil />
-        </Route>
-        <Route exact path="/creer_annonce">
-          <CreerAnnonce />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/login" />
-        </Route> */}
