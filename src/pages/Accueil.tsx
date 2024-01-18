940bf659db7ef6a56138f8b17325fc1cf315487a
Judi <judiherinirina04@gmail.com>
2024-01-18 16:46:57 +0300

    Update Accueil.tsx

--- src/pages/Accueil.tsx
+++ src/pages/Accueil.tsx
@@ -1,27 +1,45 @@
-import { IonContent,  IonFooter,  IonHeader,  IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+} from '@ionic/react';
 import Formulaire from '../components/Login/FormulaireLogin';
 import FooterTab from '../components/FooterTab';
+import { searchCircle } from 'ionicons/icons';
 
 const Accueil: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  useEffect(() => {
+    setSearchValue('Aucun annonce');
+  }, []);
+  function searchAnnonceAction(event: CustomEvent){
+    const value = event.detail.value || '' 
+    setSearchValue(value)
+  }
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Liste des annonces</IonTitle>
+          <br />
+          <IonSearchbar animated={true} searchIcon={searchCircle} placeholder="Recherche..." onIonInput={searchAnnonceAction}></IonSearchbar>
         </IonToolbar>
       </IonHeader>
 
       <IonContent>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100%',
-        }}
-      >
-        Aucun annonce
-      </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+         {searchValue}
+        </div>
       </IonContent>
     </IonPage>
   );
