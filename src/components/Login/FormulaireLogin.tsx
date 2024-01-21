71587502e46904a2f9d709911ebc5941e6729177
Judi <127020548+judi1968@users.noreply.github.com>
2024-01-21 15:56:02 +0300

    Update FormulaireLogin.tsx

--- src/components/Login/FormulaireLogin.tsx
+++ src/components/Login/FormulaireLogin.tsx
@@ -1,17 +1,13 @@
 // Formulaire.tsx
 import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Assurez-vous d'importer la feuille de style Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './../assets/css/Formulaire.css';
+import axios from 'axios';
+import API_DOMAIN from '../../config/config';
 import { useHistory } from 'react-router';
 
 const Formulaire: React.FC = () => {
   const history = useHistory();
-
-  // Fonction de gestion du clic sur le lien
-  const handleLinkClick = () => {
-    // Utilisez history.push pour naviguer vers la page désirée
-    history.push('/rooter_page');
-  }
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -23,33 +19,75 @@ const Formulaire: React.FC = () => {
     setPassword(e.target.value);
   };
 
-  const handleLogin = () => {
-    console.log('Username:', username);
-    console.log('Password:', password);
-    handleLinkClick();
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        `${API_DOMAIN}/authentification_client`,
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (response.data.status === 200) {
+        // Authentification réussie, naviguez vers la page souhaitée
+        history.push('/rooter_page');
+      } else {
+        // Gestion des erreurs d'authentification
+        console.error('Échec de l\'authentification:', response.data.message);
+      }
+    } catch (error: any) {
+      console.error('Erreur lors de la requête:', error.message);
+    }
   };
 
   return (
     <div className="container mt-5">
       <img src="%PUBLIC_URL%/../assets/images/logo/logo_trial rgba(a0).png" alt="logo du VaikaNet" />
       <form>
-      <div>
-      <div className="form-floating mb-3">
-        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-        <label htmlFor="floatingInput">Nom d'utlisateur</label>
-      </div>
-      <div className="form-floating">
-        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-        <label htmlFor="floatingPassword">Mot de passe</label>
-      </div>
-    </div>
+        <div>
+          <div className="form-floating mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+              onChange={handleUsernameChange}
+            />
+            <label htmlFor="floatingInput">Nom d'utilisateur</label>
+          </div>
+          <div className="form-floating">
+            <input
+              type="password"
+              className="form-control"
+              id="floatingPassword"
+              placeholder="Password"
+              onChange={handlePasswordChange}
+            />
+            <label htmlFor="floatingPassword">Mot de passe</label>
+          </div>
+        </div>
         <div className="text-center ">
-          <button  type="button" className="col-12 btn btn-primary" onClick={handleLogin}>
+          <button
+            type="button"
+            className="col-12 btn btn-primary"
+            onClick={handleLogin}
+          >
             Connexion
           </button>
         </div>
         <br />
-          <p>Si vous n'avez pas de compte cliquer sur <a href="/creer_compte"  className="">Creer nom nouveau compte</a></p>
+        <p>
+          Si vous n'avez pas de compte, cliquez sur{' '}
+          <a href="/creer_compte" className="">
+            Créer un nouveau compte
+          </a>
+        </p>
       </form>
     </div>
   );
