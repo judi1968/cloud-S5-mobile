2116458058e5bfdfa0770cbfa499825f467adc4b
Judi <127020548+judi1968@users.noreply.github.com>
2024-01-23 19:05:28 +0300

    Update FormulaireCreerAnnonce.tsx

--- src/components/CreerAnnonce/FormulaireCreerAnnonce.tsx
+++ src/components/CreerAnnonce/FormulaireCreerAnnonce.tsx
@@ -3,9 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './../assets/css/FormulaireCreerAnnonce.css'
 import { IonIcon } from '@ionic/react';
 import { chevronForward, handRight, star } from 'ionicons/icons';
+import API_DOMAIN from '../../config/config';
 
 const FormulaireCreerAnnonce: React.FC = () => {
     const [marqueData, setMarqueData] = useState<any>([]);
+    const [categorieData, setCategorieData] = useState<any>([]);
+    const [typeCarburantData, setTypeCarburantData] = useState<any>([]);
+    const [transmissonData, setTransmissionData] = useState<any>([]);
+    const [freinageData, setFreinageData] = useState<any>([]);
 
 
     const [marque, setMarque] = useState('');
@@ -22,7 +27,7 @@ const FormulaireCreerAnnonce: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://cloud-s5-metier-production.up.railway.app/marques', {
+        const response = await fetch(`${API_DOMAIN}/marques`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
@@ -42,22 +47,124 @@ const FormulaireCreerAnnonce: React.FC = () => {
     fetchData();
   
   }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API_DOMAIN}/categories`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data.object);
+            setCategorieData(data.object);
+          }
+        
+      } catch (error) {
+        console.error('Erreur lors de la demande au serveur:', error);
+      }
+    };
+  
+    fetchData();
+  
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API_DOMAIN}/types-carburant`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data.object);
+            setTypeCarburantData(data.object);
+          }
+        
+      } catch (error) {
+        console.error('Erreur lors de la demande au serveur:', error);
+      }
+    };
+  
+    fetchData();
+  
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API_DOMAIN}/transmissions`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data.object);
+            setTransmissionData(data.object);
+          }
+        
+      } catch (error) {
+        console.error('Erreur lors de la demande au serveur:', error);
+      }
+    };
+  
+    fetchData();
+  
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API_DOMAIN}/freinages`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data.object);
+            setFreinageData(data.object);
+          }
+        
+      } catch (error) {
+        console.error('Erreur lors de la demande au serveur:', error);
+      }
+    };
+  
+    fetchData();
+  
+  }, []);
 
   const handleMarqueChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setMarque(e.target.value);
+    setFormIncomplet(false);
+
   };
 
   const handleCategorieChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setCategorie(e.target.value);
+    setFormIncomplet(false);
+
   };
   const handleTypeCarburantChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setTypeCarburant(e.target.value);
+    setFormIncomplet(false);
+
   };
   const handleTransmissionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setTransmission(e.target.value);
+    setFormIncomplet(false);
+
   };
   const handleFreinageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setFreinage(e.target.value);
+    setFormIncomplet(false);
+
   };
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -68,13 +175,37 @@ const FormulaireCreerAnnonce: React.FC = () => {
     console.log('type carburant:',typeCarburant);
     console.log('transmission:', transmission);
     console.log('Freinage', freinage);
+    if (marque.length==0 ||
+      categorie.length==0 ||
+      categorie.length==0 ||
+      transmission.length==0 ||
+      freinage.length==0
+      ) {
+        setFormIncomplet(true)
+    }
   };
 
+  const [formIncomplete,setFormIncomplet] = useState(false);
+
+  const errorStyle = {
+    padding:'5px',
+    color:'red',
+    borderRadius:'5px',
+    border:'2px solid red'
+  }
 
   return (
     <div className="container-formulaire-creer-annonce mt-5">
       <form className="row g-3 needs-validation" noValidate onSubmit={handleSubmit}>
       <h1>Ajouter les elements de voiture</h1>
+      {formIncomplete ? 
+      <center>
+        <p style={errorStyle}> information incomplete </p> 
+      </center>
+      : ''
+      }
+
+      
       <div className="col-md">
         <div className="form-floating">
           <select 
@@ -83,7 +214,6 @@ const FormulaireCreerAnnonce: React.FC = () => {
           onChange={handleMarqueChange}
           required
           className="form-select" itemID="floatingSelectGrid" aria-label="Floating label select example">
-            <option selected>Audi</option>
              {marqueData?.map((element:any) => (
               <option value={element.id}>{element.nom}</option>
             ))} 
@@ -100,9 +230,9 @@ const FormulaireCreerAnnonce: React.FC = () => {
           onChange={handleCategorieChange}
           required
           itemID="floatingSelectGrid" aria-label="Floating label select example">
-            <option selected>Q5</option>
-            <option value="2">Q6</option>
-            <option value="3">Q7</option>
+            {categorieData?.map((element:any) => (
+              <option value={element.id}>{element.nom}</option>
+            ))} 
           </select>
           <label htmlFor="floatingSelectGrid">Categorie</label>
         </div>
@@ -116,9 +246,9 @@ const FormulaireCreerAnnonce: React.FC = () => {
           onChange={handleTypeCarburantChange}
           required
           itemID="floatingSelectGrid" aria-label="Floating label select example">
-            <option selected>carbu 1</option>
-            <option value="2">carbu 2</option>
-            <option value="3">carbu 3</option>
+            {typeCarburantData?.map((element:any) => (
+              <option value={element.id}>{element.nom}</option>
+            ))} 
           </select>
           <label htmlFor="floatingSelectGrid">Type carburant</label>
         </div>
@@ -132,9 +262,9 @@ const FormulaireCreerAnnonce: React.FC = () => {
           onChange={handleTransmissionChange}
           required
           itemID="floatingSelectGrid" aria-label="Floating label select example">
-            <option selected>trans 1</option>
-            <option value="2">trans 2</option>
-            <option value="3">trans 3</option>
+            {transmissonData?.map((element:any) => (
+              <option value={element.id}>{element.nom}</option>
+            ))}
           </select>
           <label htmlFor="floatingSelectGrid">Transmission</label>
         </div>
@@ -148,14 +278,14 @@ const FormulaireCreerAnnonce: React.FC = () => {
           onChange={handleFreinageChange}
           required
           itemID="floatingSelectGrid" aria-label="Floating label select example">
-            <option selected>frein 1</option>
-            <option value="2">frein 2</option>
-            <option value="3">frein 3</option>
+            {freinageData?.map((element:any) => (
+              <option value={element.id}>{element.nom}</option>
+            ))}
           </select>
           <label htmlFor="floatingSelectGrid">Freinage</label>
         </div>
       </div>
-
+        
         <div className="col-12">
             <button className="btn btn-primary btn-next" type="submit">Suivant     <IonIcon icon={chevronForward} /></button>
         </div>
